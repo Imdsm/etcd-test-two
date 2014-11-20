1b2077015c73f82ae8fbc3126b0af3c48b7296a2
Adam K Dean <adam@amberfish.co.uk>
2014-11-20 16:54:41 +0000

    First commit of test code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var http = require('http'),
+    etcd = new require('node-etcd')();
+
+http.createServer(function(req, res) {
+    etcd.get('test-signal', function(result) {
+        res.writeHead(200, {'Content-Type': 'text/plain' });
+        res.end('Test signal is: ' + result);    
+    });
+}).listen(80);
