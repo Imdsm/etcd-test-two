2bd8884fe3caa304a86f619aeaf26c87e7d88af3
Adam K Dean <adam@amberfish.co.uk>
2014-11-21 09:55:50 +0000

    Added console output for logging

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ var http = require('http'),
     etcd = new Etcd('127.0.0.1', '4001');
 
 http.createServer(function(req, res) {
-    etcd.get('test-signal', function(result) {
-        res.writeHead(200, {'Content-Type': 'text/plain' });
-        res.end('Test signal is: ' + result);
-    });
+    // etcd.get('test-signal', function(result) {
+    //     res.writeHead(200, {'Content-Type': 'text/plain' });
+    //     res.end('Test signal is: ' + result);
+    // });
+    etcd.get('test-signal', console.log);
+
+    res.writeHead(200, {'Content-Type': 'text/plain' });
+    res.end('Test signal is: ' + 'n/a');
 }).listen(process.env.PORT || 8000);
