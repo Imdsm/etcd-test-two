2e26593455def0b148a03e099c82c81e3c92a174
Adam K Dean <adam@amberfish.co.uk>
2014-11-21 11:35:24 +0000

    Added URL check

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ var http = require('http'),
     Etcd = new require('node-etcd'),
     etcd = new Etcd('172.17.42.1', '4001');
 
-console.log(process.env);
-
 http.createServer(function(req, res) {
-    etcd.get('test-signal', console.log);
-
-    etcd.get('test-signal', function(error, result) {
-        res.writeHead(200, {'Content-Type': 'text/plain' });
-        res.end('Test signal is: ' + result.node.value);
-    });
+    if (req.url === '/') {
+        etcd.get('test-signal', function(error, result) {
+            res.writeHead(200, {'Content-Type': 'text/plain' });
+            res.end('Test signal is: ' + result.node.value);
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain' });
+        res.end('Not found');
+    }
 }).listen(process.env.PORT || 8000);
