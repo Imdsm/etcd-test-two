ff03c57ae6912b4499e1fb15394e48e6ae3a09ae
Adam K Dean <adam@amberfish.co.uk>
2014-11-21 10:31:03 +0000

    Changes for testing

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ var http = require('http'),
 console.log(process.env);
 
 http.createServer(function(req, res) {
-    // etcd.get('test-signal', function(result) {
-    //     res.writeHead(200, {'Content-Type': 'text/plain' });
-    //     res.end('Test signal is: ' + result);
-    // });
-    console.log('getting "test-signal" value: ');
-    etcd.get('test-signal', console.log);
-
-    res.writeHead(200, {'Content-Type': 'text/plain' });
-    res.end('Test signal is: ' + 'n/a');
+    etcd.get('test-signal', function(result) {
+        res.writeHead(200, {'Content-Type': 'text/plain' });
+        res.end('Test signal is: ' + result);
+    });
 }).listen(process.env.PORT || 8000);
